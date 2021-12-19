8fb2fe7d974e2c9e86e1ab8b91dde7efc741cb76
VilleAR <vilpuurunen@gmail.com>
2021-12-19 15:36:45 +0200

    registration added but not finished

--- app.py
+++ app.py
@@ -13,11 +13,35 @@ def func(n):
     return (n**2)+5*n+17
 @app.route("/")
 def index():
-    return str(func(10))
+    return render_template("index.html")
 
 @app.route("/courses")
 def courses():
-    result = db.session.execute("SELECT prerequisites FROM courses")
-    messages = result.fetchone()[0]
-    return render_template("courses.html")
- 
+    result = db.session.execute("SELECT name FROM courses")
+    re2=db.session.execute("select professor from courses")
+    messages = result.fetchall()
+    msg2=re2.fetchall()
+    return render_template("courses.html",items=messages)
+
+@app.route("/registration")
+def registration():
+    choices=["Student", "Teacher"]
+    return render_template("registration.html", choices=choices)
+
+@app.route("/login",methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    sql = "SELECT password FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()    
+    if user==None:
+        return redirect("/")
+    else:
+        hash_value=user[0]
+        if check_password_hash(hash_value, password):
+            session["username"]=username
+            session["role"]=role
+        else:
+            return redirect("/")
+    return redirect("/")
