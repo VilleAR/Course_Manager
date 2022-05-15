6ba02e6d5f6cb04a0a462d79f18e6f3b92b92da8
VilleAR <vilpuurunen@gmail.com>
2022-05-16 01:56:06 +0300

    .

--- schema.sql
+++ schema.sql
@@ -1,3 +1,3 @@
-CREATE TABLE users (username TEXT, password TEXT, role);
+CREATE TABLE users (username TEXT, password TEXT, role TEXT);
 CREATE TABLE courses (id SERIAL PRIMARY KEY, name TEXT, professor TEXT, prerequisites INTEGER[]);
 CREATE TABLE courselists (username TEXT, courses TEXT[]);
